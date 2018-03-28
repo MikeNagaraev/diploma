1cf7736df53d2f1e8cf6965d87d08f8dea009917
Mikhail Naharayeu <Mikhail_Naharayeu@epam.com>
2018-03-28 14:18:32 +0300

    send via browser messages

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -1,28 +1,78 @@
 /*
- *  This sketch demonstrates how to scan WiFi networks. 
- *  The API is almost the same as with the WiFi Shield library, 
- *  the most obvious difference being the different file you need to include:
+ *  This sketch demonstrates how to set up a simple HTTP-like server.
+ *  The server will set a GPIO pin depending on the request
+ *    http://server_ip/gpio/0 will set the GPIO2 low,
+ *    http://server_ip/gpio/1 will set the GPIO2 high
+ *  server_ip is the IP address of the ESP8266 module, will be 
+ *  printed to Serial when the module is connected.
  */
-#include "ESP8266WiFi.h"
-#include <SoftwareSerial.h> 
-//static const uint8_t D0   = 16;
-//static const uint8_t D1   = 5;
-//static const uint8_t D2   = 4;
-//static const uint8_t D3   = 0;
-//static const uint8_t D4   = 2;
-//static const uint8_t D5   = 14;
-//static const uint8_t D6   = 12;
-//static const uint8_t D7   = 13;
-//static const uint8_t D8   = 15;
-//static const uint8_t D9   = 3; //rx
-//static const uint8_t D10  = 1; //tx
-int speed = 9600;
-SoftwareSerial* serial = new SoftwareSerial(D9, D10);
+
+#include <ESP8266WiFi.h>
+
+const char* ssid = "Mikhail_Xiomi";
+const char* password = "12345678";
+
+// Create an instance of the server
+// specify the port to listen on as an argument
+WiFiServer server(80);
+
 void setup() {
-  serial->begin(speed);
+  Serial.begin(115200);
+  delay(1000);
+
+ 
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  serial->print('\007');
-  delay(100);
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+  
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  String query = req.substring(req.indexOf("/?") + 2, req.indexOf("HTTP/1.1"));
+  Serial.println(req);
+  Serial.println(query);
+  client.flush();
+
+  // Prepare the response
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nResponse is now ";
+  s += "</html>\n";
+
+  // Send the response to the client
+  client.print(s);
+  delay(1);
+  Serial.println("Client disonnected");
+
+  // The client will actually be disconnected 
+  // when the function returns and 'client' object is detroyed
 }
+
