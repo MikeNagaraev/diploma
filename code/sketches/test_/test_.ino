fb7374d3cb88fad3ccb391c9721f341ea774df5f
MikhailLU <clowfinger13@gmail.com>
2018-04-09 02:45:30 +0300

    tl, vvedenie, start literature overview

---
+++ code/sketches/test_/test_.ino
@@ -0,0 +1,60 @@
+#include <ESP8266WiFi.h>
+ 
+const char* ssid = ""; //give yours
+const char* password = "";
+ 
+WiFiServer server(80);
+ 
+void setup() {
+  pinMode(12,OUTPUT);
+pinMode(14,OUTPUT);
+pinMode(2,OUTPUT);
+pinMode(0,OUTPUT);
+  Serial.begin(115200);
+  delay(10);
+ 
+
+ 
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+ 
+  // Print the IP address
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+ 
+}
+ 
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+ 
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+ 
+ 
+}
+ 
