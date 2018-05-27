06b31e303d2b7ed368548595a8626f3ac1ab2c1a
MikhailLU <clowfinger13@gmail.com>
2018-05-27 09:09:37 +0300

    polzovatel finish

--- code/client/mock.py
+++ code/client/mock.py
@@ -1,11 +1,2 @@
-
-addr = input("\nInput host addres: ")
-print("IP-address is not valid")
-
-
-addr = input("\nInput host addres: ")
-print("IP-address is not valid")
-
-
-addr = input("\nInput host addres: ")
-print("Welcome to main menu")
+input("\nType command: ")
+print("Good bye!")
